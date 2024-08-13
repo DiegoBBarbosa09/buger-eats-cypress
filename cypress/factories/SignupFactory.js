4ddc675ed8299757422d3c0ba5b193e199ac7f6d
DiegoBBarbosa09 <diegobrito.b09@gmail.com>
2024-08-13 15:37:08 -0300

    refatoração de testes bugereats

--- cypress/factories/SignupFactory.js
+++ cypress/factories/SignupFactory.js
@@ -1,28 +1,51 @@
-var cpf = require('gerador-validador-cpf')
+import { faker as FakerJs } from '@faker-js/faker';
+var cpf = require('gerador-validador-cpf');
 
-export default {
+export class Factory {
+  static randonArrElement(randonElemnt) {
+    const randomArrElement = FakerJs.helpers.arrayElement(randonElemnt);
+    return randomArrElement;
+  }
+}
 
-    deliver: function () {
+export class PayloadRegister {
+  static deliverMethod() {
+    const deliver = {
+      name: FakerJs.person.fullName(),
+      cpf: cpf.generate(),
+      email: FakerJs.internet.email(),
+      whatsapp: FakerJs.phone.number(),
+      address: {
+        postalcode: '06462000',
+        street: 'Rua Adoniram Barbosa',
+        number: '365',
+        details: 'casa 3',
+        district: 'Parque Imperial',
+        city_state: 'Barueri/SP',
+      },
+      delivery_method: 'Moto',
+      cnh: 'cnh-digital.jpg',
+    };
+    return deliver;
+  }
 
-        
-
-        var data = {
-            name: 'Diego Brito',
-            cpf: cpf.generate(),
-            email: 'diegobrito@gmail.com',
-            whatsapp: "11987459874",
-            address: {
-                postalcode: "06462000",
-                street: "Rua Adoniram Barbosa",
-                number: "365",
-                details: "casa 3",
-                district: "Parque Imperial",
-                city_state: "Barueri/SP"
-            },
-            delivery_method: "Moto",
-            cnh: "cnh-digital.jpg"
-
-        }
-        return data
-    }
+  static deliverMethodRandon() {
+    const deliverRandon = {
+      name: FakerJs.person.fullName(),
+      cpf: cpf.generate(),
+      email: FakerJs.internet.email(),
+      whatsapp: FakerJs.phone.number(),
+      address: {
+        postalcode: '06462000',
+        street: 'Rua Adoniram Barbosa',
+        number: '365',
+        details: 'casa 3',
+        district: 'Parque Imperial',
+        city_state: 'Barueri/SP',
+      },
+      delivery_method: 'Moto',
+      cnh: 'cnh-digital.jpg',
+    };
+    return deliverRandon;
+  }
 }
--- cypress/pages/SignupPage.js
+++ cypress/pages/SignupPage.js
@@ -1,55 +1,64 @@
-
-// modelo de escrita PascalCase - escrito com a primeira e a segunda letra das palavras em letras maiusculas
-// PascalCase é utilizado para criar class
-// modelo de escrita camelCase - escrita da primiera letra minuscula e a segunda maiuscula
-// camelCase usado para metodos e variaveis dentro da class
 class SignupPage {
-    go() {
-        cy.visit('/')
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')    
-    }
+  go() {
+    cy.get('a[href="/deliver"]').click();
+    cy.get('#page-deliver form h1').should(
+      'have.text',
+      'Cadastre-se para  fazer entregas'
+    );
+  }
 
-    fillForm(deliver) {
-                  /*************Dados Pessoais*************/
-                  cy.get('input[name="fullName"]').type(deliver.name)
-                  cy.get('input[name="cpf"]').type(deliver.cpf)
-                  cy.get('input[name="email"]').type(deliver.email)
-                  cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
-                      /*************Endereço*************/
-                  cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
-                  cy.get('input[type="button"][value="Buscar CEP"]').click()
-                  cy.get('input[name="address-number"]').type(deliver.address.number)
-                  cy.get('input[name="address-details"]').type(deliver.address.details)
-                      /*************Validação de campos de prenchimento automatico*************/
-                  cy.get('input[name="address"]').should('have.value', deliver.address.street)
-                  cy.get('input[name="district"]').should('have.value', deliver.address.district)
-                  cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
-                      /*************Metodo de entrega*************/
-                  cy.contains('.delivery-method li', deliver.delivery_method).click()
-                      /*************Anexar CNH*************/
-                  cy.get('input[accept^="image"]').attachFile('/images/'+deliver.cnh)
-                          /*    ^  ==  Começa com - pega o começo do texto
+  fillForm(deliver) {
+    /*************Dados Pessoais*************/
+    cy.get('input[name="fullName"]').type(deliver.name);
+    cy.get('input[name="cpf"]').type(deliver.cpf);
+    cy.get('input[name="email"]').type(deliver.email);
+    cy.get('input[name="whatsapp"]').type(deliver.whatsapp);
+    /*************Endereço*************/
+    cy.get('input[name="postalcode"]').type(deliver.address.postalcode);
+    cy.get('input[type="button"][value="Buscar CEP"]').click();
+    cy.get('input[name="address-number"]').type(deliver.address.number);
+    cy.get('input[name="address-details"]').type(deliver.address.details);
+    /*************Validação de campos de prenchimento automatico*************/
+    cy.get('input[name="address"]').should(
+      'have.value',
+      deliver.address.street
+    );
+    cy.get('input[name="district"]').should(
+      'have.value',
+      deliver.address.district
+    );
+    cy.get('input[name="city-uf"]').should(
+      'have.value',
+      deliver.address.city_state
+    );
+    /*************Metodo de entrega*************/
+    cy.contains('.delivery-method li', deliver.delivery_method).click();
+    /*************Anexar CNH*************/
+    cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh);
+    /*    ^  ==  Começa com - pega o começo do texto
                           cy.contains('input[accept^="image"]', entregador.cnh).click()
                                $  == Termina com - pega o final do teto 
                          cy.contains('input[accept^="image"]', entregador.cnh).click()
                               * == Contain - contem um pedaço do texto
                          cy.contains('input[accept*="image"]', entregador.cnh).click()               
                          */
-    }
+  }
 
-    submit() {
-              /*************Cadastra Usuario Moto*************/
-          cy.get('button[type="submit"]').click()
-    }
+  submit() {
+    /*************Cadastra Usuario Moto*************/
+    cy.get('button[type="submit"]').click();
+  }
 
-    modalContentShouldBe(expectedMessage) {
-        cy.get('div[id="swal2-html-container"]').should('have.text', expectedMessage)
-    }
-    alertMessageShouldBe(expectedMessage) {
-        //cy.get('.alert-error').should('have.text', expectedMessage)
-        cy.contains('.alert-error', expectedMessage).should('be.visible')
-    }
+  modalContentShouldBe(expectedMessage) {
+    cy.get('div[id="swal2-html-container"]').should(
+      'have.text',
+      expectedMessage
+    );
+  }
+  alertMessageShouldBe(expectedMessage) {
+    //cy.get('.alert-error').should('have.text', expectedMessage)
+    cy.contains('.alert-error', expectedMessage).should('be.visible');
+  }
 }
 
-export default new SignupPage;
+export default new SignupPage();
