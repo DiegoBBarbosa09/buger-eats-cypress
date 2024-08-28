31975b2f0105f5fec31655429f7ba5c1238aaceb
DiegoBBarbosa09 <DiegoBBarbosa09@users.noreply.github.com>
2024-08-28 13:55:11 +0000

    deploy: c48c36ab19cb2a051a89dc52cab4c65fd634c5d6

--- cypress/support/beforeCommands/home.js
+++
@@ -1,7 +0,0 @@
-Cypress.Commands.add('homePageOnline', () => {
-  cy.visit('/');
-  cy.get('#page-home main h1').should(
-    'have.text',
-    'Seja um parceiro entregador pela Buger Eats'
-  );
-});
